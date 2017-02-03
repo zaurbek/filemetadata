467ca0ccff03ef1596b05ad41cd9d29f7bb42651
Zaurbek <bigz3733@gmail.com>
2017-02-03 18:58:51 +0000

    Second commit

--- server.js
+++ server.js
@@ -1,10 +1,36 @@
 var express = require('express')
+
+var path=require("path")
+
 var app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+var multer=require("multer")
+var upload = multer({ dest: 'uploads/' })
+
+var port=process.env.PORT || 8080
+
+
+
+app.post('/upload', upload.single('Choose a file..'), function (req, res) {
+  res.json({
+      name: req.file.originalname,
+      filesizeBytes: req.file.size,
+      filesizeMB: req.file.size/1000000
+  })
 })
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 3000!')
+
+app.use('/assets', express.static('assets'))
+
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname+'/index.html'))
+    
+})
+
+
+
+
+app.listen(port, function () {
+  console.log('Example app listening on port 8080!')
 })
